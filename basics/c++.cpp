eadf9684e0ee36fe8958f457bc9620cd6bbb0afc
kanishk jain <doshikanishak@gmail.com>
2025-11-04 12:19:20 +0530

    stack opeerations in cpp(basic)

--- basics/c++.cpp
+++ basics/c++.cpp
@@ -758,3 +758,164 @@
 
 //===================================================================================================================================================================
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// //                                                                       DOUBLY LINKED LIST
+// class node{
+//     public:
+//     int data;
+//     node *next;
+//     node* prev;
+
+//     node(int data){
+//         this->data = data;
+//         this->next = NULL;
+//         this->prev = NULL;
+//     }
+
+
+
+
+// };
+
+// void insertatstart(node* &head,int val){ // INSERT AT START
+//     node* temp = new node(val);
+//     temp->next = head;
+//     head->prev = temp;
+//     head = temp;
+// }
+
+// void insertattail(node* &tail,int val){ // INSERT AT END
+//     node* temp = new node(val);
+//     tail->next = temp;
+//     temp->prev = tail;
+//     tail = temp;
+
+// }
+
+// void insertatposition(node* &head,node* &tail,int pos,int val){ // INSERT AT SPECIFIC POSITION
+    
+//     //insert at start
+//     if (pos == 1 || head == nullptr) {
+//         insertatstart(head,val);
+//         return;
+//     }
+    
+//     node* temp = head;
+//     int ct = 1;
+
+//     while(ct < pos - 1)
+//     {
+//         temp = temp->next;
+//         ct++;
+//     }
+
+//     //insert at last position
+//     if(temp->next == NULL){
+//         insertattail(tail,val);
+//         return;
+//     }
+
+//     node* newnode = new node(val); 
+//     newnode->next = temp->next;
+//     temp->next->prev = newnode;
+//     temp->next = newnode;
+//     newnode->prev = temp;
+    
+// }
+
+// void print(node* &head){ // TRAVERSE
+//     node* temp = head;
+//     while(temp != NULL){
+//         cout<<temp->data<<" ";
+//         temp = temp->next;
+//     }
+// }
+
+
+// int main()
+// {
+//     node* node1 = new node(10);
+
+//     node* head = node1;
+//     node* tail = node1;
+
+//     insertatstart(head,5);
+//     insertattail(tail,15);
+//     insertatposition(head,tail,3,50);
+
+//     print(head);
+
+
+
+//     return 0;
+// }
+
+//===================================================================================================================================================================
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class mystack{
+//     public:
+
+//     int* arr;
+//     int capacity;
+//     int top;
+
+//     mystack(int cap){
+//         capacity = cap;
+//         top = -1;
+//         arr = new int[capacity];
+//     }
+
+//     void push(int val){ // PUSH
+    
+//         if(top == capacity - 1){
+//             cout<<"overflow";
+//             return;
+//         }
+//         arr[++top] = val;
+//     }
+    
+//     void pop(){ // POP
+    
+//         if(top == -1){
+//             cout<<"underflow";
+//             return;
+//         }
+//         top--;
+//     }
+    
+//     int peek(){ // PEEK
+    
+//         if(top == -1){
+//             cout<<"stack is empty";
+//             return -1;
+//         }
+    
+//         return arr[top];   
+    
+//     }
+
+// };
+
+
+
+// int main()
+// {
+//     mystack s(5);
+//     s.push(1);
+//     s.push(2);
+//     s.push(3);
+//     s.push(4);
+//     s.push(5);
+
+//     cout<<"Top element: "<<s.peek()<<endl;
+
+//     s.pop();
+
+//     cout<<"Top after pop: "<<s.peek();
+    
+//     return 0;
+// }
