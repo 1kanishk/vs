1823bf83965af435e04fcb418679545c9c695cc4
kanishk jain <doshikanishak@gmail.com>
2026-01-02 21:18:42 +0530

    EDA

--- basics/cpp_dsa_practice.cpp
+++ basics/cpp_dsa_practice.cpp
@@ -2,21 +2,6 @@
 using namespace std;
 int main()
 {
-    int arr[] = {6, 4, 10};
-    int len = sizeof(arr) / sizeof(arr[0]);
-    int sum = 0, maxsum = 0;
-
-    for (int i = 0; i < len; i++)
-    {
-
-        if (sum < 0)
-        {
-            sum = 0;
-        }
-
-        maxsum = max(maxsum, sum);
-        cout << maxsum;
-    }
-
+    cout<<"vdfsv";
     return 0;
 }
